223d3c80741af22719be29deb66b0383164cfbc9
Angela Mutava <angela.mutava@andela.com>
2018-01-17 10:41:15 +0300

    [project configuration] added the project configuration.

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+   
+    render() {
+        return (
+            <div>
+                <p> Hello wergasdf---------sdfasfvWorld!</p>
+            </div>
+        )
+
+    }
+}
+export default App;
