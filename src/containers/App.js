9b2ebabde029550836e7eff5790e3d4889943d7e
Angela Mutava <angela.mutava@andela.com>
2018-01-22 14:58:03 +0300

    correct error on app load.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import Navigation from '../common/navbar/Navigation';
-import ChatBot from '../components/chat/ChatBot';
-import Body from '../components/body/Body';
-import ChatApp from '../components/messaging/ChatApp';
-import './Header.css';
 
 
-class App extends Component {
-    
-   
+
+class App extends Component { 
     render() {
         return (
            <div className="body"> 
-            <section className= "body1">
-                <Navigation/>
-                <Body/>
-            </section>
-            <section className= "body2"> 
-                <ChatBot/>
-                <ChatApp/>
-            
-            </section>
+           <p>we have just began</p>
         </div>
         )
 
